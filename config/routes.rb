a9a9fd8f5783a21cd2791a2759bd1ad7e672e2b6
Gregory Kan <kan.gregory@gmail.com>
2017-10-23 11:23:47 +1300

    chore(routes): make characters index the root

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   resources :lastnames
   resources :firstnames
   resources :characters
+  root 'characters#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
